6af55533dee171f3d41a414b57e38e4b46a8ad34
Ruxshona <133767416+ruxshonazakiryayeva@users.noreply.github.com>
2026-08-18 14:39:10 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
-
 import './globals.css'
-export const metadata = { title: 'WebInvite v2 - O'tkan kunlar' }
+export const metadata = { title: "WebInvite v2 - O'tkan kunlar" }
 export default function RootLayout({children}:{children:React.ReactNode}){
   return <html lang="uz"><body>{children}</body></html>
 }
